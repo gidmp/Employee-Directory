72f6c5db4d0c31e5b4b164e9bee44ec7e4bbd60b
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-07-18 16:51:00 -0700

    Updates

---
+++ precache-manifest.89ea6eff1d5d054d88bad4acc5b73d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1745d772870885bff2d62cb20375e51",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "6893902424ea2f6bd056",
+    "url": "/Employee-Directory/static/css/main.a106fad6.chunk.css"
+  },
+  {
+    "revision": "c0aae143a87736da74c8",
+    "url": "/Employee-Directory/static/js/2.14b6af6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.14b6af6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6893902424ea2f6bd056",
+    "url": "/Employee-Directory/static/js/main.47a012d4.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
